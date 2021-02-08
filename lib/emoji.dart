5fffa5deb78132efab282f65a6969774ac4c34b0
ggdream <gdream@yeah.net>
2021-02-08 15:29:14 +0800

    emoji表情

---
+++ lib/emoji.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart' show IconData;
+
+
+class IconEmoji {
+  static const IconData pub           = IconData(0xe611, fontFamily: 'IconEmoji', matchTextDirection: true);
+  static const IconData settle        = IconData(0xe614, fontFamily: 'IconEmoji', matchTextDirection: true);
+  static const IconData kiss          = IconData(0xe615, fontFamily: 'IconEmoji', matchTextDirection: true);
+  static const IconData cute          = IconData(0xe616, fontFamily: 'IconEmoji', matchTextDirection: true);
+  static const IconData cool          = IconData(0xe617, fontFamily: 'IconEmoji', matchTextDirection: true);
+  static const IconData cry           = IconData(0xe619, fontFamily: 'IconEmoji', matchTextDirection: true);
+  static const IconData cry_loudly    = IconData(0xe61a, fontFamily: 'IconEmoji', matchTextDirection: true);
+  static const IconData awkward       = IconData(0xe61c, fontFamily: 'IconEmoji', matchTextDirection: true);
+  static const IconData shyness       = IconData(0xe620, fontFamily: 'IconEmoji', matchTextDirection: true);
+  static const IconData sadness       = IconData(0xe622, fontFamily: 'IconEmoji', matchTextDirection: true);
+}
--- lib/flutter_icons.dart
+++ lib/flutter_icons.dart
@@ -1,7 +1,4 @@
 library flutter_icons;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+
+export 'emoji.dart';
